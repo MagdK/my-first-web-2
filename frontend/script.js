a4940033bc91005958ac3856f51fdd3cbe2f9e8b
MagdK <kovacsmagda@gmail.com>
2022-01-19 18:45:18 +0100

    Add function

---
+++ frontend/script.js
@@ -0,0 +1,17 @@
+// szolj()
+// window.szolj()
+function szolj() {
+    console.log("My web is loaded 2.1")
+    uzenet()
+    uzenet()
+}
+window.addEventListener("load", szolj)
+
+function uzenet(){
+    console.log("Effit ki kell loni a Holdra")
+    kaja()
+}
+
+function kaja(){
+    console.log("Birkaporkolt")
+}
