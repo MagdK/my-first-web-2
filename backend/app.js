d6bcbcf169e4a8a14d662b98f7f7f9effe16f034
Magda Kovács <kovacsmagda@gmail.com>
2022-04-15 18:06:38 +0200

    Unnecesary rows commented

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
 const port = 9001;
 const express = require('express');
-const fs = require('fs');
+
 const path = require('path');
 const app = express();
 
 // express.json() is a built in middleware function in Express. It parses incoming JSON requests and puts the parsed data in req.body.
-app.use(express.json());
+// app.use(express.json());
 
 const frontendFolder = path.join(__dirname, '../frontend');
 const publicFolder = path.join(frontendFolder, 'pub');
