4c1982637d5248be916ce47cd5201a48ffaf00a8
Magda Kovács <kovacsmagda@gmail.com>
2022-04-15 19:05:20 +0200

    Add two button

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,54 @@
 const port = 9001;
 const express = require('express');
-
+const fs = require('fs');
 const path = require('path');
 const app = express();
 
 // express.json() is a built in middleware function in Express. It parses incoming JSON requests and puts the parsed data in req.body.
-// app.use(express.json());
+app.use(express.json());
 
 const frontendFolder = path.join(__dirname, '../frontend');
 const publicFolder = path.join(frontendFolder, 'pub');
+// join a perjelet hozzaadja
+const dataFolder = path.join(__dirname, 'data');
+const subscriberFile = path.join(dataFolder, 'subscribers.json')
 // adds a middleware for serving static files to your Express app.
 // elso parameter a use-ban az az, hogy az url ben mivel kezdodik, masodik meg az eleresi utvonal
 app.use('/pub', express.static(publicFolder));
 
-// join a perjelet hozzaadja
-const dataFolder = path.join(__dirname, 'data');
+
 console.log(dataFolder);
-app.use('/api', express.static(dataFolder));
+// app.use('/api', express.static(dataFolder));
 
 app.get('/subscribers', (req, res) => {
     res.sendFile(path.join(frontendFolder, 'index.html'));
+});
+
+app.get('/subscribers/all', (req, res) => {
+    fs.readFile(subscriberFile, (error, data) => {
+        if (error) {
+            res.send("Something went a bit wrong")
+        } else {
+            const subscribers = JSON.parse(data)
+            const cards = subscribers.cards
+            res.send(cards);
+    
+        }
+    })
+});
+
+app.get('/subscribers/under25', (req, res) => {
+    fs.readFile(subscriberFile, (error, data) => {
+        if (error) {
+            res.send("Something went very wrong")
+        } else {
+            const subscribers = JSON.parse(data)
+            const cards = subscribers.cards
+            const subscribersUnderTwentyfive = cards.filter(subscriber => subscriber.age < 25)
+            res.send(subscribersUnderTwentyfive)
+    
+        }
+    })
 });
 
 
--- frontend/pub/script.js
+++ frontend/pub/script.js
@@ -15,16 +15,31 @@ function pageContent(subscribers) {
     return myStr;
 };
 
+function pageHeader() {
+    return`
+        <div>
+            <h1>Subscribers</hs>
+            <button class="all-btn">All</button>
+            <button class="under25-btn">Under 25</button>
+         </div>
+    `;
+};
+
 async function fetchSubscriber() {
-    const subscriberData = await fetch(`http://127.0.0.1:9001/api/subscribers.json`);
+    const subscriberData = await fetch(`http://127.0.0.1:9001/subscribers/all`);
     return subscriberData.json();
 };
 
+
 async function loadEvent() {
     const subscribers = await fetchSubscriber();
-
     const rootElement = document.getElementById('root');
-    rootElement.innerHTML = pageContent(subscribers.cards);
+    
+    // const allBtn = document.querySelector(".all-btn");
+    // const under25Btn = document.querySelector(".under25-btn");
+
+    rootElement.insertAdjacentHTML("beforebegin", pageHeader());
+    rootElement.innerHTML = pageContent(subscribers);
 };
 
 window.addEventListener("load", loadEvent);
