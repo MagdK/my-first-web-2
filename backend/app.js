b9f0acd6b373df0ba5260e4d1bc734f2b2cc360e
MagdK <kovacsmagda@gmail.com>
2022-04-16 16:23:57 +0200

    start a new app/server

--- backend/app.js
+++ backend/app.js
@@ -8,63 +8,12 @@ const app = express();
 app.use(express.json());
 
 const frontendFolder = path.join(__dirname, '../frontend');
-const publicFolder = path.join(frontendFolder, 'pub');
-// join a perjelet hozzaadja
-const dataFolder = path.join(__dirname, 'data');
-const subscriberFile = path.join(dataFolder, 'subscribers.json')
-const archivedSubscriberFile = path.join(dataFolder, 'archived-subscribers.json')
-// adds a middleware for serving static files to your Express app.
-// elso parameter a use-ban az az, hogy az url ben mivel kezdodik, masodik meg az eleresi utvonal
-app.use('/pub', express.static(publicFolder));
+const publicFolder = path.join(__dirname, '../frontend/pub');
+const dataFolder = path.join(__dirname, '/data')
 
-
-console.log(dataFolder);
-// app.use('/api', express.static(dataFolder));
-
-app.get('/subscribers', (req, res) => {
-    res.sendFile(path.join(frontendFolder, 'index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(frontendFolder)
 });
-
-app.get('/subscribers/all', (req, res) => {
-    fs.readFile(subscriberFile, (error, data) => {
-        if (error) {
-            res.send("Something went a bit wrong")
-        } else {
-            const subscribers = JSON.parse(data)
-            const cards = subscribers.cards
-            res.send(cards);
-    
-        }
-    })
-});
-
-app.get('/subscribers/under25', (req, res) => {
-    fs.readFile(subscriberFile, (error, data) => {
-        if (error) {
-            res.send("Something went very wrong")
-        } else {
-            const subscribers = JSON.parse(data)
-            const cards = subscribers.cards
-            const subscribersUnderTwentyfive = cards.filter(subscriber => subscriber.age < 25)
-            res.send(subscribersUnderTwentyfive)
-    
-        }
-    })
-});
-
-app.get('/subscribers/archived', (req, res) => {
-    fs.readFile(archivedSubscriberFile, (error, data) => {
-        if (error) {
-            res.send("Something went a tiny bit wrong")
-        } else {
-            const subscribers = JSON.parse(data)
-            const cards = subscribers.cards
-            res.send(cards);
-        }
-    })
-});
-
-
 
 app.listen(port, () => {
     console.log(`http://127.0.0.1:${port}`)
